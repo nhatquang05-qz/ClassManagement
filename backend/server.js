7db69ba22628bbf90922fc798559c7abd0743b50
Nhat_Quang <23521278@gm.uit.edu.vn>
2026-01-03 16:21:36 +0700

    config for depl

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*', 
+    credentials: true 
+}));
